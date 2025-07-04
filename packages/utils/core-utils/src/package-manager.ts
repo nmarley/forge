9453d15116ace021eb322b89860c76e6a61ff82d
Nathan Marley <nathan@glowberrylabs.com>
2025-07-04 11:30:46 -0500

    Add support for bun package manager

--- packages/utils/core-utils/src/package-manager.ts
+++ packages/utils/core-utils/src/package-manager.ts
@@ -8,7 +8,7 @@ import logSymbols from 'log-symbols';
 
 const d = debug('electron-forge:package-manager');
 
-export type SupportedPackageManager = 'yarn' | 'npm' | 'pnpm';
+export type SupportedPackageManager = 'yarn' | 'npm' | 'pnpm' | 'bun';
 export type PMDetails = { executable: SupportedPackageManager; version?: string; install: string; dev: string; exact: string };
 
 let hasWarned = false;
@@ -35,12 +35,20 @@ export const PACKAGE_MANAGERS: Record<SupportedPackageManager, PMDetails> = {
     dev: '--save-dev',
     exact: '--save-exact',
   },
+  bun: {
+    executable: 'bun',
+    install: 'add',
+    dev: '--dev',
+    exact: '--exact',
+  },
 };
 
 const PM_FROM_LOCKFILE: Record<string, SupportedPackageManager> = {
   'package-lock.json': 'npm',
   'yarn.lock': 'yarn',
   'pnpm-lock.yaml': 'pnpm',
+  'bun.lock': 'pnpm',
+  'bun.lockb': 'bun',
 };
 
 /**
@@ -72,13 +80,20 @@ function pmFromUserAgent() {
  *
  * The version of the executing package manager is also returned if it is detected via user agent.
  *
- * Supported package managers are `yarn`, `pnpm`, and `npm`.
+ * Supported package managers are `yarn`, `pnpm`, `bun`, and `npm`.
  *
  */
 export const resolvePackageManager: () => Promise<PMDetails> = async () => {
   const executingPM = pmFromUserAgent();
   let lockfilePM;
-  const lockfile = await findUp(['package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'pnpm-workspace.yaml'], { type: 'file' });
+  const lockfile = await findUp([
+    'package-lock.json',
+    'yarn.lock',
+    'pnpm-lock.yaml',
+    'pnpm-workspace.yaml',
+    'bun.lock',
+    'bun.lockb',
+  ], { type: 'file' });
   if (lockfile) {
     const lockfileName = path.basename(lockfile);
     lockfilePM = PM_FROM_LOCKFILE[lockfileName];
@@ -95,6 +110,7 @@ export const resolvePackageManager: () => Promise<PMDetails> = async () => {
     case 'yarn':
     case 'npm':
     case 'pnpm':
+    case 'bun':
       d(
         `Resolved package manager to ${installer}. (Derived from NODE_INSTALLER: ${process.env.NODE_INSTALLER}, npm_config_user_agent: ${process.env.npm_config_user_agent}, lockfile: ${lockfilePM})`
       );
